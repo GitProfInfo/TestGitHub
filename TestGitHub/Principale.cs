da29ae8d6f39a125d83986cff139a25faa29cae0
GitProfInfo <43937139+GitProfInfo@users.noreply.github.com>
2021-05-18 17:14:35 +0200

    Aggiungere i file di progetto.

---
+++ TestGitHub/Principale.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitHub
+{
+    class Principale
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Title = "Test GitHub";
+
+            Console.WriteLine("...");
+            Console.ReadKey();
+        }
+    }
+}
